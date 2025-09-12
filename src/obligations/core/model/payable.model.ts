5af5170aa969bd3af6bc92c8d7c4b33c3352ca5f
Walefe Pereira <walefedev@gmail.com>
2025-09-12 10:40:20 -0300

    feat: add payable model

---
+++ src/obligations/core/model/payable.model.ts
@@ -0,0 +1,25 @@
+import { randomUUID } from 'node:crypto';
+
+type WithOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+export class PayableModel {
+  declare id: string;
+  declare value: number;
+  declare emissionDate: Date;
+  declare assignor: string;
+
+  private constructor(data: PayableModel) {
+    Object.assign(this, data);
+  }
+
+  static create(data: WithOptional<PayableModel, 'id'>): PayableModel {
+    return new PayableModel({
+      ...data,
+      id: randomUUID(),
+    });
+  }
+
+  static createFrom(data: PayableModel): PayableModel {
+    return new PayableModel(data);
+  }
+}
