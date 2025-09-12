e7f3514dda5b3bb1aa9245572e6f3ed9e465bee4
Walefe Pereira <walefedev@gmail.com>
2025-09-12 10:43:02 -0300

    chore: import new module 'payable' to app module load and remove unnecessary imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { PrismaService } from './prisma.service';
+import { ObligationsModule } from '@src/obligations/obligations.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [ObligationsModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
