9fa6bd5e04fdec93f360a30c38c8c2ed3e44f3ae
Walefe Pereira <walefedev@gmail.com>
2025-09-21 16:14:16 -0300

    feat: add migration and model to assignor and payable

---
+++ src/prisma/migrations/20250921190855_add_assignor_and_payable_tables/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "public"."payable" (
+    "id" TEXT NOT NULL,
+    "value" DOUBLE PRECISION NOT NULL,
+    "emission_date" TIMESTAMP(3) NOT NULL,
+    "assignor_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "payable_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."assignor" (
+    "id" TEXT NOT NULL,
+    "document" VARCHAR(30) NOT NULL,
+    "email" VARCHAR(140) NOT NULL,
+    "phone" VARCHAR(20) NOT NULL,
+    "name" VARCHAR(140) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "assignor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "payable_assignor_id_key" ON "public"."payable"("assignor_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "assignor_email_key" ON "public"."assignor"("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."payable" ADD CONSTRAINT "payable_assignor_id_fkey" FOREIGN KEY ("assignor_id") REFERENCES "public"."assignor"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
