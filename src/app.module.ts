4293e4f9517a9a294a69a35caf8d653712b27813
Walefe Pereira <walefedev@gmail.com>
2025-09-21 16:19:47 -0300

    chore: remove prisma service from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { PrismaService } from './prisma.service';
 import { ObligationsModule } from '@src/obligations/obligations.module';
 
 @Module({
   imports: [ObligationsModule],
-  providers: [PrismaService],
 })
 export class AppModule {}
