9bdd4f6825656c8f1dc4f0a51cb084eb6ae26b8a
Walefe Pereira <walefedev@gmail.com>
2025-09-23 15:16:52 -0300

    feat: config swagger and scalar for docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Ibank')
+    .setDescription('A simple api')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  app.use('/reference', apiReference({ content: documentFactory }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
