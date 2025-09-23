ff327cc589957089ff0ffeff7fc3e90e6ca0a233
Walefe Pereira <walefedev@gmail.com>
2025-09-23 15:15:13 -0300

    feat: add config module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { ObligationsModule } from '@src/obligations/obligations.module';
+import { ConfigModule } from './obligations/infra/module/config/config.module';
 
 @Module({
-  imports: [ObligationsModule],
+  imports: [ConfigModule.forRoot(), ObligationsModule],
 })
 export class AppModule {}
