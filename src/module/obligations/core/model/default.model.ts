0795efbcf597c734a2480d71543db2936d316094
Walefe Pereira <walefedev@gmail.com>
2025-10-16 22:35:02 -0300

    chore: remove from old directory

--- src/module/obligations/core/model/default.model.ts
+++
@@ -1,8 +0,0 @@
-export type WithOptional<T, K extends keyof T> = Omit<T, K> &
-  Partial<Pick<T, K>>;
-
-export abstract class DefaultModel {
-  declare readonly id: string;
-  declare createdAt: Date;
-  declare updatedAt: Date;
-}
